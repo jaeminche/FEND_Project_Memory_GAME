dc105003bb371df443841d4b0a35c8df7ca5c171
Jae Min Choi <jaeminche@gmail.com>
2017-11-04 14:11:14 +0900

    feat: Add rateStar(), starCounter(), a popup, and startTimer()

--- js/app.js
+++ js/app.js
@@ -67,26 +67,33 @@ $(".restart").click(function() {
 
 let openShowCard = function(cardSelected) {
 	$(cardSelected).addClass("open show");
+	$(cardSelected).css("pointer-events", "none");
 	pushCardInList(cardSelected);
 }
 
 let listOfCardClassNm = [];
-let listOfSeltCard = [];
-let listMatched = [];
+let listForMatch = [];
+let totalMatch = 0;
 let pushCardInList = function(cardSltd) {
 	var classNmOfCard = $(cardSltd).children().attr('class');
 	listOfCardClassNm.push(classNmOfCard);
-	listOfSeltCard.push(cardSltd);
+	listForMatch.push(cardSltd);
 	if (listOfCardClassNm.length === 2) {
 		incrementMoveCnt();
 		if (listOfCardClassNm[0] === listOfCardClassNm[1]) {
-			$(listOfSeltCard).addClass("match");
+			$(listForMatch).addClass("match");
+			totalMatch++;
+			if (totalMatch === 8) {
+				starCounter();
+				openPopup();
+			}
+			$(listForMatch).off('click');
 			listOfCardClassNm = [];
 		} else {
 			$(".card").css("pointer-events", "none");
 			setTimeout(function() {
-				$(listOfSeltCard).removeClass("open show");
-				listOfSeltCard = [];
+				$(listForMatch).removeClass("open show");
+				listForMatch = [];
 				$(".card").css("pointer-events", "auto");
 			}, 700);
 			listOfCardClassNm = [];
@@ -97,15 +104,39 @@ let pushCardInList = function(cardSltd) {
 // let mat = new Match();
 
 let incrementMoveCnt = function() {
-	var numMove = $(".moves");
+	let numMove = $(".moves");
 	numMove.html(parseInt(numMove.html()) + 1);
-	// mat.openShowCard();
+	if (parseInt(numMove.html()) >= 12) {
+		rateStar(numMove.html());
+	}
 }
+// let starsNum = 3;
+let rateStar = function(numTry) {
+	let target = $(".third-star");
+	// let num = '2';
+	if (numTry === '15') {
+		target = $(".second-star");
+		// num = '1';
+	}
+	if (numTry === '18') {
+		target = $(".first-star");
+		// $('.num-stars').html("0");
+		// num = '0';
+	}
+	target.removeClass('fa-star').addClass('fa-star-o');
+	// $('.num-stars').html("num");
+}
+
 
 $(".card").click(function() {
 	openShowCard(this);
 });
 
+let starCounter = function() {
+	let list = [];
+	list = $('.fa-star');
+	$('.num-stars').html(list.length);
+}
 
 
 // const listShow = [];
@@ -144,3 +175,88 @@ $(".card").click(function() {
  * Work on the winning condition. How does your game “know” if a player has won?
  * We recommend saving styling until the very end. Allow your game logic and functionality to dictate the styling.
  */
+
+
+
+ // Get the popup
+var popup = document.getElementById('myPopup');
+
+// Get the button that opens the popup
+// var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the popup
+var span = document.getElementsByClassName("close")[0];
+
+// When the user wins the game, open the popup
+let openPopup = function() {
+    popup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the popup
+span.onclick = function() {
+    popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
+
+
+
+//plain timer
+// var minutesLabel = document.getElementById("minutes");
+// var secondsLabel = document.getElementById("seconds");
+// var totalSeconds = 0;
+// setInterval(setTime, 1000);
+
+// function setTime()
+// {
+//     ++totalSeconds;
+//     secondsLabel.innerHTML = pad(totalSeconds%60);
+//     minutesLabel.innerHTML = pad(parseInt(totalSeconds/60));
+// }
+
+// function pad(val)
+// {
+//     var valString = val + "";
+//     if(valString.length < 2)
+//     {
+//         return "0" + valString;
+//     }
+//     else
+//     {
+//         return valString;
+//     }
+// }
+//plain timer ends
+
+
+
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+$(function ($) {
+    let easyMode = 60 * 5,
+    	mediumMode = 60 * 2,
+    	extremeMode = 60 * 1,
+    	impossibleMode = 60 * 0.5,
+        display = $('#count-down');
+    startTimer(impossibleMode, display);
+});
